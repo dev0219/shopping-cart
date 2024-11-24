1222748a09686b9412c99895f8077707a1a9e995
Imad <rhs0219@hotmail.com>
2024-11-24 11:01:20 -0400

    3 updates

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -67,7 +67,7 @@ const Cart = () => {
                 <div className="shopping-list-item__action-wrapper">
                   <div className="shopping-list-item__action-wrapper-counter">
                     <div className="increment-decrement counter">
-                      <div style={{ fontSize: "15px" }} className="counter__trash counter-button" onClick={() => changeQuantity(index, 'decrease')}>
+                      <div className="counter__trash counter-button" onClick={() => changeQuantity(index, 'decrease')}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
                           <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
                           <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
@@ -75,7 +75,7 @@ const Cart = () => {
                       </div>
                       <span className="counter__quantity">{pro.quantity}</span>
                       <span className="state--loading hidden"><span></span><span></span><span></span><span></span></span>
-                      <div style={{ fontSize: "15px" }} className="counter__increment counter-button" onClick={() => changeQuantity(index, 'increase')}>
+                      <div className="counter__increment counter-button" onClick={() => changeQuantity(index, 'increase')}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
                           <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
                         </svg>
